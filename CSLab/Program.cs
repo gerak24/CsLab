d8fbe56846e84ff6293165a6dc5d098daa9ca051
gerak24 <gerak24032003@yandex.ru>
2024-12-04 14:32:55 +0300

    Init

---
+++ CSLab/Program.cs
@@ -0,0 +1,17 @@
+﻿
+CircleCalc();
+
+void CircleCalc()
+{
+    Console.WriteLine("Введите радиус окружности:");
+
+    var rad = float.Parse(Console.ReadLine() ?? "0");
+    while (rad == 0)
+    {
+        Console.WriteLine("Введите радиус окружности:");
+        rad = float.Parse(Console.ReadLine() ?? "0");
+    }
+
+    Console.WriteLine($"Площадь крга = {Math.PI * double.Pow(rad, 2)}");
+    Console.WriteLine($"Площадь сферы = {4.0 / 3.0 * Math.PI * double.Pow(rad, 3)}");
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+The usual cpp calculator. 
+Available operands: + - * /
+To exit, you need to press 'q' after the calculation
