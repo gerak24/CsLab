242652c12258081bfbe45c7d3b85dc33d531c7b3
gerak24 <gerak24032003@yandex.ru>
2024-12-09 16:36:13 +0300

    Init lab2 func

--- CSLab/Lab2/Lab2.cs
+++ CSLab/Lab2/Lab2.cs
@@ -1,11 +1,12 @@
 ﻿namespace CSLab.Lab2;
 
-public  abstract class Lab2 : Lab2Quests
+public abstract class Lab2 : Lab2Quests
 {
     public static void Menu()
     {
         Console.WriteLine("Лабораторная 1:");
-        Console.WriteLine("Введите цифру от 0 до 9, для выбора задания. (d - для выполнение дополнительного общего задания)");
+        Console.WriteLine(
+            "Введите цифру от 0 до 9, для выбора задания. (d - для выполнение дополнительного общего задания)");
         Console.WriteLine("Введите \"Q\" для выхода в главное меню");
         var pointer = Console.ReadKey().KeyChar;
         while (true)
@@ -15,6 +16,7 @@ public  abstract class Lab2 : Lab2Quests
             {
                 case '0':
                     Console.WriteLine("Определить, является ли введенное число четным.");
+                    Multiplicity(2);
                     break;
                 case '1':
                     Console.WriteLine("Определить, является ли заданное число двузначным числом, оканчивающимся на 3.");
@@ -24,28 +26,30 @@ public  abstract class Lab2 : Lab2Quests
                     Console.WriteLine("Вклад < 100 — 5%\0Вклад > 100 но < 200 — 7 %\0Вклад > 200 — 10%\0");
                     break;
                 case '3':
-                    Console.WriteLine("Вычислить сумму и произведение чисел произвольного трехзначного числа.");
+                    Console.WriteLine("Нахождение корней квадратного уравнения.");
                     break;
                 case '4':
-                    Console.WriteLine("Вычислить значение функции y=f(x).");
+                    Console.WriteLine("День недели по числу.");
                     break;
                 case '5':
-                    Console.WriteLine("Вычислить значение выражения.");
+                    Console.WriteLine("Решение задачи.");
                     break;
                 case '6':
-                    Console.WriteLine("Найти сумму и произведение цифр четырехзначного числа.");
+                    Console.WriteLine("Проверка на кратность 3.");
+                    Multiplicity(3);
                     break;
                 case '7':
-                    Console.WriteLine("Вывести  наибольшее из целых чисел A и B.");
-                    break;  
+                    Console.WriteLine("Вычисление значения функции.");
+                    break;
                 case '8':
-                    Console.WriteLine("Вывести  наибольшее из целых чисел A и B.");
-                    break;    
+                    Console.WriteLine("Начисление скидки.");
+                    Console.WriteLine("Сумма < 18000 — 1%\0Сумма > 18000 но < 30000 — 5%\0Сумма > 30000 — 10%\0");
+                    break;
                 case '9':
-                    Console.WriteLine("Вывести  наибольшее из целых чисел A и B.");
+                    Console.WriteLine("Скидка по рейтингу покупателя.");
                     break;
                 case 'd':
-                    Console.WriteLine("Вычислить длину окружности, площадь круга и объем сферы по заданному радиусу.");
+                    Console.WriteLine("Проверка знания таблицы умножения.");
                     break;
                 case 'Q':
                     return;
--- CSLab/Lab2/Lab2Quests.cs
+++ CSLab/Lab2/Lab2Quests.cs
@@ -1,6 +1,25 @@
-﻿namespace CSLab.Lab2;
+﻿using static System.Int32;
 
-public class Lab2Quests
+namespace CSLab.Lab2;
+
+public abstract class Lab2Quests
 {
-    
+    protected static void Multiplicity(int brief)
+    {
+        var success = false;
+        while (!success)
+        {
+            Console.WriteLine("Введите число:");
+            success = TryParse(Console.ReadLine(), out var digits);
+            switch (digits % brief == 0)
+            {
+                case true : 
+                        Console.WriteLine($"Число кратно {brief}");
+                    break;
+                case false : 
+                    Console.WriteLine($"Число не кратно {brief}");
+                    break;
+            }
+        }
+    }
 }
