883eb30d2a65ca4c714024718b19a43a5bf96614
gerak24 <gerak24032003@yandex.ru>
2024-12-16 13:52:15 +0300

    Finish lab3

--- CSLab/Lab3/Lab3.cs
+++ CSLab/Lab3/Lab3.cs
@@ -11,45 +11,63 @@ public abstract class Lab3 : Lab3Quests
         while (true)
         {
             Console.WriteLine();
+            List<int> results;
             switch (pointer)
             {
-                case '1':
+                case '0':
                     Console.WriteLine("Вывод всех чётных чисел от 1 до 100, их количества и суммы.");
-                    EvenCheck();
+                    results = MultiplicityAnalyze([1, 100], 2);
+                    foreach (var result in results)
+                        Console.Write($"{result} ");
+                    Console.WriteLine($"Сумма чётных чисел = {results.Sum()}");
+                    Console.WriteLine($"Количество чётных чисел = {results.Count}");
                     break;
-                case '2':
+                case '1':
                     Console.WriteLine("Вывод всех чисел, оканчивающихся на 5 в заданном промежутке");
                     foreach (var result in EndCharAnalyze('5'))
                         Console.Write($"{result} ");
                     Console.WriteLine();
                     break;
-                case '3':
+                case '2':
                     Console.WriteLine("Вывести максимальное число, кратное 5, в заданном ряду.");
-                    Console.WriteLine($"Максимальное число, кратное 5 = {EndCharAnalyze('5').Max()}");
+                    Console.WriteLine(
+                        $"Максимальное число, кратное 5 = {MultiplicityAnalyze(EnterNums(), 5).Max()}");
+                    break;
+                case '3':
+                    Console.WriteLine("Определить, является ли число простым");
+                    Console.WriteLine(EasyNum(EnterNum("Введите число: "))
+                        ? "Число является простым"
+                        : "Число не является простым");
                     break;
                 case '4':
-                    Console.WriteLine("Определить, является ли число простым");
+                    Console.WriteLine("«Угадай число!»");
+                    Guessing();
                     break;
                 case '5':
-                    Console.WriteLine("«Угадай число!»");
+                    Console.WriteLine("«Тренажер таблицы умножения»");
+                    MultyTrainer();
                     break;
                 case '6':
-                    Console.WriteLine("«Тренажер таблицы умножения»");
+                    Console.WriteLine("«Все простые числа в диапазоне»");
+                    EasyNums();
                     break;
                 case '7':
-                    Console.WriteLine("Вывод всех чисел, оканчивающихся на 6 в заданном промежутке, их сумма и количество");
-                    var results = EndCharAnalyze('6');
+                    Console.WriteLine(
+                        "Вывод всех чисел, оканчивающихся на 6 в заданном промежутке, их сумма и количество");
+                    results = EndCharAnalyze('6');
                     foreach (var result in results)
                         Console.Write($"{result} ");
                     Console.WriteLine();
-                    Console.WriteLine($"Сумма чисел = {results.Sum()}");
-                    Console.WriteLine($"Количество чётных чисел = {results.Count}");
+                    Console.WriteLine($"Сумма = {results.Sum()}");
+                    Console.WriteLine($"Количество = {results.Count}");
                     break;
                 case '8':
                     Console.WriteLine("Сумма вклада с ежемесячным процентом.");
+                    PercentCalc();
                     break;
                 case '9':
                     Console.WriteLine("Таблица умножения");
+                    MultyTable();
                     break;
                 case 'Q':
                     return;
--- CSLab/Lab3/Lab3Quests.cs
+++ CSLab/Lab3/Lab3Quests.cs
@@ -2,17 +2,7 @@
 
 public abstract class Lab3Quests
 {
-    protected static void EvenCheck()
-    {
-        var results = MultiplicityAnalyze(1, 100, 2);
-        foreach (var result in results)
-            Console.Write($"{result} ");
-        Console.WriteLine();
-        Console.WriteLine($"Сумма чётных чисел = {results.Sum()}");
-        Console.WriteLine($"Количество чётных чисел = {results.Count}");
-    }
-
-    protected static List<int> EndCharAnalyze(char ends)
+    protected static List<int> EnterNums()
     {
         var success = false;
         int from = 0, to = 0;
@@ -28,17 +18,41 @@ public abstract class Lab3Quests
                 Console.WriteLine("Промежуток должен быть задан целым числом.");
         }
 
+        return [from, to];
+    }
+
+    protected static int EnterNum(string text)
+    {
+        var success = false;
+        var num = 0;
+        while (!success)
+        {
+            Console.WriteLine(text);
+            success = int.TryParse(Console.ReadLine(), out num);
+        }
+
+        return num;
+    }
+
+    protected static List<int> EndCharAnalyze(char ends)
+    {
+        var nums = EnterNums();
+        var from = nums[0];
+        var to = nums[1];
         var results = new List<int>();
         for (var i = from; i <= to; i++)
         {
             if (i.ToString().Last() == ends)
                 results.Add(i);
         }
+
         return results;
     }
 
-    private static List<int> MultiplicityAnalyze(int from, int to, int multyplier)
+    protected static List<int> MultiplicityAnalyze(List<int> nums, int multyplier)
     {
+        var from = nums[0];
+        var to = nums[1];
         List<int> results = [];
         for (var i = from; i <= to; i++)
         {
@@ -47,5 +61,131 @@ public abstract class Lab3Quests
         }
 
         return results;
+    }
+
+    protected static bool EasyNum(int num)
+    {
+        if (num is <= 1 and >= -1) return false;
+        for (var i = 2; i < int.Abs(num); i++)
+        {
+            if (num % i != 0) continue;
+            return false;
+        }
+
+        return true;
+    }
+
+    protected static void EasyNums()
+    {
+        var nums = EnterNums();
+        var from = nums[0];
+        var to = nums[1];
+        for (var i = from; i < to; i++)
+        {
+            if (EasyNum(i)) Console.Write($"{i} ");
+        }
+
+        Console.WriteLine();
+    }
+
+    protected static void Guessing()
+    {
+        while (true)
+        {
+            var num = Random.Shared.Next(0, 100);
+            var pointer = EnterNum("Введите число: ");
+            while (pointer != num)
+            {
+                Console.WriteLine(pointer > num ? "Перелёт!" : "Недолёт!");
+                pointer = EnterNum("Введите число: ");
+            }
+
+            Console.WriteLine("Молодец! В точку!");
+            Console.WriteLine("Начать заново? Y\\N");
+            var qqpoint = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+            while (qqpoint != 'Y' && qqpoint != 'N' && qqpoint != 'y' && qqpoint != 'n')
+            {
+                Console.WriteLine("Начать заново? Y\\N");
+                qqpoint = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+            }
+
+            switch (qqpoint)
+            {
+                case 'Y' or 'y':
+                    continue;
+                case 'N' or 'n':
+                    return;
+            }
+
+            break;
+        }
+    }
+
+    protected static void MultyTrainer()
+    {
+        int count = 0, right = 0;
+        while (true)
+        {
+            var first = Random.Shared.Next(1, 9);
+            var second = Random.Shared.Next(1, 9);
+            Console.WriteLine($"{first}*{second} = ");
+            var num = EnterNum("Введите число: ");
+            count++;
+            if (num == first * second)
+            {
+                right++;
+                Console.WriteLine("Верно");
+            }
+            else
+                Console.WriteLine("Неверно");
+
+            Console.WriteLine("Продолжить? Y\\N");
+            var qqpoint = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+            while (qqpoint != 'Y' && qqpoint != 'N' && qqpoint != 'y' && qqpoint != 'n')
+            {
+                Console.WriteLine("Продолжить? Y\\N");
+                qqpoint = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+            }
+
+            switch (qqpoint)
+            {
+                case 'Y' or 'y':
+                    continue;
+                case 'N' or 'n':
+                    Console.WriteLine($"Всего попыток – {count}");
+                    Console.WriteLine($"Верных ответов – {right} ({Math.Round((decimal)right / count * 100)})%");
+                    Console.WriteLine(
+                        $"Неверных – {count - right} ({Math.Round(100 - (decimal)right / count * 100)}%).");
+                    return;
+            }
+        }
+    }
+
+    protected static void PercentCalc()
+    {
+        double sum = EnterNum("Введите сумму вклада: ");
+        var months = EnterNum("Введите количество месяцев: ");
+        for (var i = 0; i < months; i++)
+        {
+            sum += sum * 0.02;
+        }
+
+        Console.WriteLine($"Сумма на счету к концу срока: {sum}");
+    }
+
+    protected static void MultyTable()
+    {
+        for (var first = 1; first < 10; first++)
+        {
+            for (var second = 1; second < 10; second++)
+            {
+                Console.Write($"{first * second} ");
+            }
+            Console.WriteLine();
+        }
     }
 }
