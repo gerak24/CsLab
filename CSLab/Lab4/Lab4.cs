8f4cae785bc87934688379cc46fe4485217b0dd7
gerak24 <gerak24032003@yandex.ru>
2024-12-18 17:20:04 +0300

    init 4 lab

--- CSLab/Lab4/Lab4.cs
+++ CSLab/Lab4/Lab4.cs
@@ -1,9 +1,62 @@
 ﻿namespace CSLab.Lab4;
 
-public abstract class Lab4
+public abstract class Lab4 : Lab4Quests
 {
     public static void Menu()
     {
-        Console.Clear();
+        Console.WriteLine("Лабораторная 4:");
+        Console.WriteLine("Введите цифру от 1 до 7, для выбора задания.");
+        Console.WriteLine("Введите \"Q\" для выхода в главное меню");
+
+        var pointer = Console.ReadKey().KeyChar;
+        while (true)
+        {
+            Console.WriteLine();
+            switch (pointer)
+            {
+                case '0':
+                    Console.WriteLine("Массив 10 случайных натуральных чисел в диапазоне от 0 до 100");
+                    var arr = CreateRandArray(10, 0, 100);
+                    Console.Write("Массив: ");
+                    foreach (var num in arr)
+                        Console.Write($"{num} ");
+                    Console.WriteLine();
+                    ArrAnalyze(arr);
+                    break;
+                case '1':
+                    Console.WriteLine("Определить, является ли расположение чисел в массиве упорядоченным ");
+                    break;
+                case '2':
+                    Console.WriteLine("Вычислить сумму и произведение чисел произвольного трехзначного числа.");
+                    break;
+                case '3':
+                    Console.WriteLine("Вычислить значение функции y=f(x).");
+                    break;
+                case '4':
+                    Console.WriteLine("Вычислить значение выражения.");
+                    break;
+                case '5':
+                    Console.WriteLine("Найти сумму и произведение цифр четырехзначного числа.");
+                    break;
+                case '6':
+                    Console.WriteLine("Вывести  наибольшее из целых чисел A и B.");
+                    break;
+                case '7':
+                    Console.WriteLine("Вывести  наибольшее из целых чисел A и B.");
+                    break;
+                case '8':
+                    Console.WriteLine("Вывести  наибольшее из целых чисел A и B.");
+                    break;
+                case '9':
+                    Console.WriteLine("Вывести  наибольшее из целых чисел A и B.");
+                    break;
+                case 'Q':
+                    return;
+            }
+
+            Console.WriteLine("Для продолжения выберите задание введя цифру 1-7.");
+            Console.WriteLine("Введите \"Q\" для выхода в главное меню");
+            pointer = Console.ReadKey().KeyChar;
+        }
     }
 }
--- CSLab/Lab4/Lab4Quests.cs
+++ CSLab/Lab4/Lab4Quests.cs
@@ -2,5 +2,54 @@
 
 public class Lab4Quests
 {
-    
+    public static int[] CreateRandArray(int length, int from, int to)
+    {
+        var result = new int[length];
+        for (var i = 0; i < length; i++)
+        {
+            var random = new Random().Next(from, to);
+            result[i] = random;
+        }
+
+        return result;
+    }
+
+    public static void ArrAnalyze(int[] array)
+    {
+        int multyply = 1,
+            maxcounter = 0,
+            mincounter = 0,
+            evencount = 0,
+            nevencount = 0,
+            zerocount = 0,
+            fivemcount = 0,
+            customcount = 0;
+        for (var i = 0; i < array.Length; i++)
+        {
+            multyply *= array[i];
+            if (array[i] >= array.Max())
+                maxcounter = i;
+            else if (array[i] <= array.Min())
+                mincounter = i;
+            if (array[i] % 2 == 0 && array[i] != 0)
+                evencount++;
+            else if (array[i] == 0)
+                zerocount++;
+            else
+                nevencount++;
+            if (array[i] % 5 == 0)
+                fivemcount++;
+            if (array[i] % 3 == 0 && array[i].ToString().Last() == '9')
+                customcount++;
+        }
+
+        Console.WriteLine($"Наибольший элемент = {array.Max()}, под индексом: {maxcounter}");
+        Console.WriteLine($"Наименьший элемент = {array.Min()}, под индексом: {mincounter}");
+        Console.WriteLine(
+            $"Количество элементов массива: {array.Length}, их сумма: {array.Sum()}, их произведение {multyply}");
+        Console.WriteLine(
+            $"Количество чётных элементов массива: {evencount}. Количество нечётных элементов массива: {nevencount}. Количество нулевых элементов массива: {zerocount}");
+        Console.WriteLine($"Количество элементов массива кратных 5: {fivemcount}");
+        Console.WriteLine($"Количество элементов массива кратных 3 и заканчивающихся на 9: {customcount}");
+    }
 }
