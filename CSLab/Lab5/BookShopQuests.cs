21f94cd871880c1e22dcbd31ec707176af8a8ced
gerak24 <gerak24032003@yandex.ru>
2025-01-09 14:00:50 +0300

    Fix 5 lab

--- CSLab/Lab5/BookShopQuests.cs
+++ CSLab/Lab5/BookShopQuests.cs
@@ -6,13 +6,14 @@ namespace CSLab.Lab5;
 // ReSharper disable once InconsistentNaming
 public class BookShopQuests
 {
-    protected static void BookIndex(IEnumerable<Book> books)
+    protected static void BookIndex(List<Book> books)
     {
         Console.WriteLine("--------------------------------------------------------------------");
         foreach (var book in books)
             Console.WriteLine(
                 $"{book.Name}: Автор - {book.Author}, Стоимость - {book.Price}, Год выпуска - {book.Year}");
         Console.WriteLine("--------------------------------------------------------------------");
+        Console.WriteLine($"Общая стоимость: {books.Select(x => x.Price).Sum()}");
     }
 
     protected static Book EnterBook(List<Book> books)
@@ -21,7 +22,7 @@ public class BookShopQuests
         var name = Console.ReadLine() ?? string.Empty;
         Console.WriteLine("Введите имя автора книги:");
         var author = Console.ReadLine() ?? string.Empty;
-        var year = SharedFunctions.EnterInt("Введите год выпуска игрока:");
+        var year = SharedFunctions.EnterInt("Введите год издания:");
         var price = (decimal)SharedFunctions.EnterDouble("Введите стоимость книги:");
         var number = Random.Shared.Next(1, 100000);
         while (books.Any(b => b.InventoryNum == number))
@@ -31,9 +32,9 @@ public class BookShopQuests
         return new Book(number, name, author, year, price);
     }
 
-    protected static void CostFilter(IEnumerable<Book> team)
+    protected static void CostFilter(IEnumerable<Book> books)
     {
         var maxCost = (decimal)SharedFunctions.EnterDouble("Введите максимальную стоимость искомых книг:");
-        BookIndex(team.Where(b => b.Price <= maxCost));
+        BookIndex(books.Where(b => b.Price <= maxCost).ToList());
     }
 }
--- CSLab/Lab5/Lab5.cs
+++ CSLab/Lab5/Lab5.cs
@@ -1,44 +1,44 @@
-﻿using CSLab.Lab5.Entities;
+﻿    using CSLab.Lab5.Entities;
 
-namespace CSLab.Lab5;
+    namespace CSLab.Lab5;
 
-public abstract class Lab5 : BookShopQuests
-{
-    private static List<Book> _products = [];
-
-    public static void Menu()
+    public abstract class Lab5 : BookShopQuests
     {
-        Console.WriteLine("Лабораторная 5:");
-        Console.WriteLine("\"Книжный магазин\".");
-        Console.WriteLine("1 - Список книг.");
-        Console.WriteLine("2 - Добавить книгу.");
-        Console.WriteLine("3 - Фильтровать по стоимости.");
-        Console.WriteLine("Введите \"Q\" для выхода в меню лабораторной №4");
+        private static List<Book> _products = [];
 
-        var pointer = Console.ReadKey().KeyChar;
-        while (true)
+        public static void Menu()
         {
-            Console.WriteLine();
-            switch (pointer)
-            {
-                case '1':
-                    BookIndex(_products);
-                    break;
-                case '2':
-                    _products.Add(EnterBook(_products));
-                    break;
-                case '3':
-                    CostFilter(_products);
-                    break;
-                case 'Q':
-                case 'q':
-                    return;
-            }
+            Console.WriteLine("Лабораторная 5:");
+            Console.WriteLine("\"Книжный магазин\".");
+            Console.WriteLine("1 - Список книг.");
+            Console.WriteLine("2 - Добавить книгу.");
+            Console.WriteLine("3 - Фильтровать по стоимости.");
+            Console.WriteLine("Введите \"Q\" для выхода в меню лабораторной №5");
 
-            Console.WriteLine(
-                "Чтобы продолжить выберите действие:\n 1 - Список игроков. \n 2 - Добавить игрока. \n 3 - Количество игроков выбранного амплуа. \n 4 - Количество совершеннолетних игроков.");
-            Console.WriteLine("Введите \"Q\" для выхода в меню лабораторной №4");
-            pointer = Console.ReadKey().KeyChar;
+            var pointer = Console.ReadKey().KeyChar;
+            while (true)
+            {
+                Console.WriteLine();
+                switch (pointer)
+                {
+                    case '1':
+                        BookIndex(_products);
+                        break;
+                    case '2':
+                        _products.Add(EnterBook(_products));
+                        break;
+                    case '3':
+                        CostFilter(_products);
+                        break;
+                    case 'Q':
+                    case 'q':
+                        return;
+                }
+
+                Console.WriteLine(
+                    "Чтобы продолжить выберите действие:\n 1 - Список книг. \n 2 - Добавить книгу. \n 3 - Фильтровать по стоимости.");
+                Console.WriteLine("Введите \"Q\" для выхода в меню лабораторной №5");
+                pointer = Console.ReadKey().KeyChar;
+            }
         }
     }
-}
